511bd73e552702a7602f4bb55b180b2cf9b37cfb
pranavgadekar <ppgadekar.92@gmail.com>
2016-03-13 19:26:28 -0400

    Fuctionalities added using FreeTTS and PDFBox

--- PDFTextToSpeech/src/PDFtoSpeech.java
+++ PDFTextToSpeech/src/PDFtoSpeech.java
@@ -1,23 +1,36 @@
 import java.io.File;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import com.sun.speech.freetts.*;
+import java.io.*;
+
 
 
 public class PDFtoSpeech {
 
 	public static void main(String[] args) {
-		PDDocument inputDocument = null;
+		PDDocument pdDocument;
+		Voice voice;
+		VoiceManager voiceManager;
 		try{
 			File file = new File("CrackCodeInterviews.pdf");
-			inputDocument = PDDocument.load(file);
+			pdDocument = PDDocument.load(file);
 			
-			PDDocumentInformation inputDocInfo = inputDocument.getDocumentInformation();
-			System.out.println(inputDocument.getNumberOfPages());
-			System.out.println(inputDocInfo.getAuthor());
+			PDFTextStripper pdfTextStripper = new PDFTextStripper();
+			String inputText = pdfTextStripper.getText(pdDocument);
+			
+			voiceManager = VoiceManager.getInstance();
+			voice = voiceManager.getVoice("kevin16");
+			
+			voice.allocate();
+			voice.speak(inputText);
+					
+			pdDocument.close();
 		}
 		catch(Exception e){
-			
+			e.printStackTrace();
 		}
 
 	}
